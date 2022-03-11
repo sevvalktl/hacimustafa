755935ea7d3f52e3a143085c3f021ce4667a8c0b
sevvalktl <94388976+sevvalktl@users.noreply.github.com>
2022-03-11 13:25:32 +0300

    new project

---
+++ home/main.js
@@ -0,0 +1,19 @@
+$(function(){
+    $('.home-slider').owlCarousel();
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        dots:false,
+        nav:false,
+        responsive:{
+            0:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+            
+        }
+    })
+});
